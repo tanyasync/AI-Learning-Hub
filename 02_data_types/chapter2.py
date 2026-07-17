4182eb2bcf9a62cc4539984a4e6e6c0e0e11a13a
Tanya Tiwari <tanyatiwari9505@gmail.com>
2026-07-17 23:04:22 +0530

    updated code

---
+++ 02_data_types/chapter2.py
@@ -0,0 +1,8 @@
+spice_mix=set()
+print(f"Initial spice mix id: {id(spice_mix)}")
+
+spice_mix.add("ginger")
+spice_mix.add("cardomom")
+
+print(f"Initial spice mix id: {spice_mix}")
+print(f"Second initial spice mix id: {id(spice_mix)}")
